ac26a30cc9c0368a7103264e0f4ed5ecd96a5cc4
Zhack <mesbah.zacharia@sfr.fr>
2025-12-25 18:56:44 +0100

    ICARE 100 augmented

--- examples/Hecktor22.py
+++ examples/Hecktor22.py
@@ -23,10 +23,10 @@ augmented=True
 
 #model_name = "FS_SVM"
 #model_name = "icare10"
-#model_name = "icare100"
+model_name = "icare100"
 #model_name = "cox"
 #model_name = "rsf10"
-model_name = "rsf100"
+#model_name = "rsf100"
 
 if augmented:
     csv_file = open(f"../csvs/Perf_Hecktor_augmented_{model_name}.csv", "w")
