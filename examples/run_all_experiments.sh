4bfbe1cfb903871d2a75b6bfd75693575c27a463
Zhack <mesbah.zacharia@sfr.fr>
2026-02-18 16:55:53 +0100

    ADD: sh script to run all experiments

---
+++ examples/run_all_experiments.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for i in icare10 icare100 FS-SVM cox rsf100 rsf10 gb10 gb100
+do
+  for j in True False
+  do
+    echo Hecktor22 $i $j
+    ../../venv/bin/python3 Hecktor22.py $i $j
+  done
+done
